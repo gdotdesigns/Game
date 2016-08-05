c74277324674ee24594df3c16a263104917f1f11
gdotdesigns <gdotdesigns@gmail.com>
2016-08-04 20:34:55 -0500

    Implemented GameStateManager to manage game states(menu, play.....)

--- core/src/net/gdotdesigns/game/Game.java
+++ core/src/net/gdotdesigns/game/Game.java
@@ -6,37 +6,43 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import net.gdotdesigns.game.States.GameStateManager;
+import net.gdotdesigns.game.States.MenuState;
+
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	public static final int WIDTH=480;
+
+    public static final int WIDTH=480;
 	public static final int HEIGHT=800;
+
 	public static final String TITLE = "Game";
+
 	private static final int MAX_FPS = 30;
     private static final int MAX_FRAMES_SKIPPED=5;
     private static final int FRAME_PERIOD =1000/MAX_FPS;
+
+    private GameStateManager gsm;
+    private SpriteBatch batch;
 
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		//test
-		//test #2
+        gsm = new GameStateManager();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
