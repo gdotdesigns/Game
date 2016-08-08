9081565376d8d502e9adb586e7f19f4dcee3d454
gdotdesigns <gdotdesigns@gmail.com>
2016-08-07 21:51:33 -0500

    Trying animation of Sprites from textureatlas

--- core/src/net/gdotdesigns/game/Game.java
+++ core/src/net/gdotdesigns/game/Game.java
@@ -3,8 +3,16 @@ package net.gdotdesigns.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GLTexture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import net.gdotdesigns.game.States.GameStateManager;
 import net.gdotdesigns.game.States.MenuState;
@@ -20,29 +28,59 @@ public class Game extends ApplicationAdapter {
     private static final int MAX_FRAMES_SKIPPED=5;
     private static final int FRAME_PERIOD =1000/MAX_FPS;
 
-    private GameStateManager gsm;
+	private final float WORLD_WIDTH=100;
+	private final float WORLD_HEIGHT=200;
+
+    //private GameStateManager gsm;
     private SpriteBatch batch;
+	private TextureAtlas textureAtlas;
+	private Animation animation;
+	private float elapsedTime;
+	private OrthographicCamera cam;
+	private Viewport vp;
 
 
 	@Override
 	public void create () {
+		float aspectRatio =(float)Gdx.graphics.getHeight()/(float)Gdx.graphics.getWidth();
+		cam =new OrthographicCamera(aspectRatio*100,aspectRatio*200);
+		//cam.position.set(cam.viewportWidth/2,cam.viewportHeight/2,0);
+		vp=new ScreenViewport(cam);
+		vp.apply();
 		batch = new SpriteBatch();
-        gsm = new GameStateManager();
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        gsm.push(new MenuState(gsm));
+		//GLTexture.setEnforcePotImages(false);
+		textureAtlas = new TextureAtlas("greenguy.txt");
+		animation = new Animation(1/6f,textureAtlas.getRegions());
+        //gsm = new GameStateManager();
+        Gdx.gl.glClearColor(0, 0,0, 1);
+
+        //gsm.push(new MenuState(gsm));
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		//super.resize(width, height);
+		vp.update(width,height);
+		cam.position.set(cam.viewportWidth/2,cam.viewportHeight/2,0);
 	}
 
 	@Override
 	public void render () {
 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.render(batch);
+        //gsm.update(Gdx.graphics.getDeltaTime());
+		//gsm.render(batch);
+		batch.setProjectionMatrix(cam.combined);
+		batch.begin();
+		elapsedTime+=Gdx.graphics.getDeltaTime();
+		batch.draw(animation.getKeyFrame(elapsedTime,true),0,0,0,0,34,43,10,10,90);
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		textureAtlas.dispose();
 
 	}
 }
