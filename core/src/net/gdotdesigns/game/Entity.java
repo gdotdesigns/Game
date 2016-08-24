09b16be3e787a47152c9ddd24ceb656c3faa7c5b
gdotdesigns <gdotdesign@gmail.com>
2016-08-24 14:48:31 -0500

    Implementing collision detection.

--- core/src/net/gdotdesigns/game/Entity.java
+++ core/src/net/gdotdesigns/game/Entity.java
@@ -1,11 +1,21 @@
 package net.gdotdesigns.game;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
 /**
  * Created by Todd on 8/22/2016.
  */
 public class Entity {
 
+    Sprite sprite;
+    Body body;
+    World world;
+
     public Entity(){
+
+        body.setUserData(this);
 
     }
 }
---
+++ core/src/net/gdotdesigns/game/EntityCollision.java
@@ -0,0 +1,31 @@
+package net.gdotdesigns.game;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+/**
+ * Created by Todd on 8/24/2016.
+ */
+public class EntityCollision implements ContactListener {
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
