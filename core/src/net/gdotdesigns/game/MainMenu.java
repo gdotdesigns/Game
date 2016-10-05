7a323a9431ab6efb3e125c74e9e2c541d72214f0
gdotdesigns <gdotdesigns@gmail.com>
2016-10-04 22:28:35 -0500

    Implementing Main Menu. Learning Scene2d.ui , skins, and JSON.

--- core/src/net/gdotdesigns/game/MainMenu.java
+++ core/src/net/gdotdesigns/game/MainMenu.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -29,6 +32,7 @@ public class MainMenu implements Screen{
     Skin skin;
     TextButton start;
     TextButton stop;
+    ImageButton imageButton;
     Label gameTitle;
     FillViewport fillViewport;
 
@@ -76,6 +80,8 @@ public class MainMenu implements Screen{
             }
         });
 
+        imageButton = new ImageButton(skin.getDrawable("games_achievements_green"),skin.getDrawable("games_achievements"));
+
         table.setFillParent(true);
         table.setDebug(true);
         table.top();
@@ -85,6 +91,8 @@ public class MainMenu implements Screen{
         table.add(start);
         table.row();
         table.add(stop);
+        table.row();
+        table.add(imageButton);
         stage.addActor(table);
         Gdx.input.setInputProcessor(stage);
 
