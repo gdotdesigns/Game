ce25ad399f57bc33dfe3988c0deba566054e358d
gdotdesigns <gdotdesigns@gmail.com>
2016-10-12 22:26:01 -0500

    Implementing Main Menu. Learning Scene2d.ui , skins, and JSON.

--- core/src/net/gdotdesigns/game/Assets.java
+++ core/src/net/gdotdesigns/game/Assets.java
@@ -1,6 +1,7 @@
 package net.gdotdesigns.game;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /**
@@ -14,7 +15,9 @@ public class Assets {
     public AssetManager manager = new AssetManager();
 
 
-    public void loadMenuAssets() {manager.load(MENU_SKIN, Skin.class);} //This automatically loads the corresponding atlas file with the same name as JSON file...
+    //public void loadMenuAssets() {manager.load(MENU_SKIN, Skin.class);} //This automatically loads the corresponding atlas file with the same name as JSON file...
+
+   public void loadMenuAssets() {manager.load(MENU_SKIN, Skin.class, new SkinLoader.SkinParameter("GameAssets64.atlas"));}
 
     public Skin getMenuAssets(){return manager.get(MENU_SKIN,Skin.class);}
 
