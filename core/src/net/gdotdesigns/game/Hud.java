3376478c671efbaae3f0742acabaedc21855a078
gdotdesigns <gdotdesigns@gmail.com>
2016-10-14 22:56:35 -0500

    Implementing Main Menu. Learning Scene2d.ui , skins, and JSON.

--- core/src/net/gdotdesigns/game/Hud.java
+++ core/src/net/gdotdesigns/game/Hud.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
