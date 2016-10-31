47b3995dee65c159e9438e07bc11f32031f1df04
gdotdesigns <gdotdesigns@gmail.com>
2016-10-31 12:55:43 -0500

    Saving encrypted score. Preparation for Google play services.

--- android/src/net/gdotdesigns/game/AndroidLauncher.java
+++ android/src/net/gdotdesigns/game/AndroidLauncher.java
@@ -11,10 +11,13 @@ import net.gdotdesigns.game.Game;
 public class AndroidLauncher extends AndroidApplication {
 
 		private InterstitialAd interstitialAd;
+		private static final String INTERSTITIAL_UNTI_AD ="ca-app-pub-2895382750471159~5396822225";
+		private static final String TEST_DEVICE= "";
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         interstitialAd = new InterstitialAd(this);
+		interstitialAd.setAdUnitId(INTERSTITIAL_UNTI_AD);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MainGameScreen(), config);
 	}
