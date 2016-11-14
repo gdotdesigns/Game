59bb5488615e9b9894fe159a311dc0bf9b801070
gdotdesigns <gdotdesigns@gmail.com>
2016-11-13 18:47:31 -0600

    Saving encrypted score. Preparation for Google play services.

--- desktop/src/net/gdotdesigns/game/desktop/DesktopLauncher.java
+++ desktop/src/net/gdotdesigns/game/desktop/DesktopLauncher.java
@@ -5,15 +5,17 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import net.gdotdesigns.game.AdController;
+import net.gdotdesigns.game.GooglePlayServices;
 import net.gdotdesigns.game.MainGameScreen;
 
-public class DesktopLauncher implements AdController{
+public class DesktopLauncher implements AdController ,GooglePlayServices {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width= MainGameScreen.WIDTH;
 		config.height =MainGameScreen.HEIGHT;
 		config.title=MainGameScreen.TITLE;
-		new LwjglApplication(new MainGameScreen(new DesktopLauncher()), config);
+		new LwjglApplication(new MainGameScreen(new DesktopLauncher(),new DesktopLauncher()), config);
 	}
 
 	@Override
@@ -21,6 +23,50 @@ public class DesktopLauncher implements AdController{
         Gdx.app.postRunnable(runnable);
     }
 
+	@Override
+	public boolean isSignedInGPGS() {
+		return false;
+	}
+
+	@Override
+	public void signInGPGS() {
+
+	}
+
+	@Override
+	public void submitScoreGPGS(int score, String id) {
+
+	}
+
+	@Override
+	public void unlockAchievementGPGS(String achievementId) {
+
+	}
+
+	@Override
+	public void getLeaderboardGPGS(String id) {
+
+	}
+
+	@Override
+	public void getAchievementsGPGS() {
+
+	}
+
+	@Override
+	public void signOutGPGS() {
+
+	}
+
+	@Override
+	public void disconnectGPGS() {
+
+	}
+
+	@Override
+	public boolean getConnectionStatus() {
+		return false;
+	}
 }
 
 
