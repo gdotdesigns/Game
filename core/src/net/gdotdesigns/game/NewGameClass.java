804e7daa140758adc5c2bacd72e4ef86270841cf
gdotdesigns <gdotdesigns@gmail.com>
2016-11-22 18:58:11 -0600

    FrameWork Complete. Making minor tweaks to framework. Working on core game.

--- core/src/net/gdotdesigns/game/NewGameClass.java
+++ core/src/net/gdotdesigns/game/NewGameClass.java
@@ -1,12 +1,36 @@
 package net.gdotdesigns.game;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by Todd on 11/22/2016.
  */
 
 public class NewGameClass implements Screen {
+
+    Assets assets;
+    OrthographicCamera camera;
+    SpriteBatch spriteBatch;
+    EntityManager entityManager;
+    SaveState saveScore;
+    MainGameScreen mainGameScreen;
+    MainMenu mainMenu;
+
+
+    public NewGameClass(Assets assets, SpriteBatch spriteBatch, MainGameScreen mainGameScreen, MainMenu mainMenu){
+        this.assets=assets;
+        this.camera= new OrthographicCamera();
+        this.spriteBatch=spriteBatch;
+        this.entityManager=new EntityManager();//Made an object instead of a static class due to Android issues with glitched textures...
+        saveScore = new SaveState();
+        this.mainGameScreen=mainGameScreen;
+        this.mainMenu=mainMenu;
+
+
+
+    }
     @Override
     public void show() {
 
