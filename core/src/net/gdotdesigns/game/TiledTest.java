a878a39a934ea9979549922c02992db1f3eab08a
gdotdesigns <gdotdesigns@gmail.com>
2016-12-14 21:56:16 -0600

    FrameWork Complete. Making minor tweaks to framework. Working on core game.

--- core/src/net/gdotdesigns/game/TiledTest.java
+++ core/src/net/gdotdesigns/game/TiledTest.java
@@ -1,8 +1,12 @@
 package net.gdotdesigns.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 /**
  * Created by Todd on 11/22/2016.
@@ -10,28 +14,42 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TiledTest implements Screen {
 
-
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
     OrthographicCamera camera;
 
 
 
     public TiledTest(){
 
-        this.camera= new OrthographicCamera();
+
+
     }
 
     @Override
     public void show() {
+        camera= new OrthographicCamera();
+        TmxMapLoader loader = new TmxMapLoader();
+        map = loader.load("data/maps/tiledtest/map.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
 
     }
 
     @Override
     public void render(float delta) {
 
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.setView(camera);
+        renderer.render();
+
     }
 
     @Override
     public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
 
     }
 
@@ -52,6 +70,7 @@ public class TiledTest implements Screen {
 
     @Override
     public void dispose() {
-
+        map.dispose();
+        renderer.dispose();
     }
 }
